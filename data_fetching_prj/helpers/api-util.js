7f943932f8786ad3ba4a7c2b22653328ef1328f9
wskim <kws60000@naver.com>
2022-11-01 12:14:23 +0900

    TIL: 서버 사이드 렌더링 변환

--- data_fetching_prj/helpers/api-util.js
+++ data_fetching_prj/helpers/api-util.js
@@ -1,6 +1,6 @@
 export async function getAllEvents() {
     const response = await fetch(
-        "https://nextjs-course-2460a-default-rtdb.firebaseio.com/events.json"
+        'https://nextjs-course-2460a-default-rtdb.firebaseio.com/events.json'
     );
     const data = await response.json();
 
@@ -24,4 +24,20 @@ export async function getFeaturedEvents() {
 export async function getEventById(id) {
     const allEvetns = await getAllEvents();
     return allEvetns.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+    const { year, month } = dateFilter;
+
+    const allEvetns = await getAllEvents();
+
+    let filteredEvents = allEvetns.filter((event) => {
+        const eventDate = new Date(event.date);
+        return (
+            eventDate.getFullYear() === year &&
+            eventDate.getMonth() === month - 1
+        );
+    });
+
+    return filteredEvents;
 }
--- data_fetching_prj/pages/events/index.js
+++ data_fetching_prj/pages/events/index.js
@@ -1,12 +1,13 @@
-import React, { Fragment } from "react";
-import { getAllEvents } from "../../dummy-data";
-import EventList from "../../components/events/event-list";
-import EventSearch from "../../components/events/events-search";
-import { useRouter } from "next/router";
+import React, { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
-export default function AllEventsPage() {
+import { getAllEvents } from '../../helpers/api-util';
+import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
+
+function AllEventsPage(props) {
     const router = useRouter();
-    const events = getAllEvents();
+    const events = { props };
 
     const findEventsHandler = (year, month) => {
         const fullPath = `/events/${year}/${month}`;
@@ -17,15 +18,19 @@ export default function AllEventsPage() {
         <Fragment>
             <EventSearch onSearch={findEventsHandler} />
             <EventList items={events} />
-            {/* <iframe
-                width="200"
-                height="200"
-                src="https://www.youtube.com/embed/fl2TrA0l-Xg"
-                title="곰주먹 김정균 vs 비밀병희 임병희 [블랙컴뱃03]"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-            ></iframe> */}
         </Fragment>
     );
 }
+
+export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+        props: {
+            events: events,
+        },
+        revalidate: 60,
+    };
+}
+
+export default AllEventsPage;
