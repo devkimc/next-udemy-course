31bab262e8f2ff9ab4c264e20529cce2d620855e
linkrws <kws60000@naver.com>
2022-11-03 19:00:07 +0900

    TIL: mongo db 연결

--- api_routes_prj/pages/api/comments/[eventId].js
+++ api_routes_prj/pages/api/comments/[eventId].js
@@ -1,19 +1,16 @@
 function handler(req, res) {
     const eventId = req.query.eventId;
 
-    if (req.method === "POST") {
-        // add server-side validation
-
-        const { email, name, text } = req.body;
-
+    if (req.method === 'POST') {
+        const { email, name, text } = JSON.parse(req.body);
         if (
-            !email.includes("@") ||
+            !email.includes('@') ||
             !name ||
-            name.trim() === "" ||
+            name.trim() === '' ||
             !text ||
-            text.trim() === ""
+            text.trim() === ''
         ) {
-            res.status(422).json({ message: "Invalid Input." });
+            res.status(422).json({ message: 'Invalid Input.' });
             return;
         }
 
@@ -28,15 +25,15 @@ function handler(req, res) {
         console.log(newComment);
 
         res.status(201).json({
-            message: "Added comment.",
+            message: 'Added comment.',
             comment: newComment,
         });
     }
 
-    if (req.method === "GET") {
+    if (req.method === 'GET') {
         const dummyList = [
-            { id: "c1", name: "Max", text: "A first commit!" },
-            { id: "c2", name: "Manuel", text: "A second commit!" },
+            { id: 'c1', name: 'Max', text: 'A first commit!' },
+            { id: 'c2', name: 'Manuel', text: 'A second commit!' },
         ];
 
         res.status(200).json({
--- api_routes_prj/pages/api/newsletter.js
+++ api_routes_prj/pages/api/newsletter.js
@@ -1,15 +1,25 @@
+import { MongoClient } from 'mongodb';
+
 function handler(req, res) {
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
         const userEmail = req.body.email;
 
-        if (!userEmail || !userEmail.includes("@")) {
-            res.status(422).json({ message: "Invalid email address" });
+        if (!userEmail || !userEmail.includes('@')) {
+            res.status(422).json({ message: 'Invalid email address' });
             return;
         }
 
+        MongoClient.connect(
+            'mongodb+srv://wskim:dRO3O6P2vV68P4MH@cluster0.c4wznsy.mongodb.net/?retryWrites=true&w=majority'
+        ).then((client) => {
+            const db = client.db();
+            db.collection('emails');
+            console.log(`connect ${client}`);
+        });
+
         console.log(userEmail);
 
-        res.status(201).json({ message: "Signed up!" });
+        res.status(201).json({ message: 'Signed up!' });
     }
 }
 
