3a711d3e5c91a807c2db06d67b8948ad4bf1adcc
wskim <kws60000@naver.com>
2022-11-05 20:47:56 +0900

    TIL: 초기 프로젝트 생성

---
+++ context/components/input/comment-list.js
@@ -0,0 +1,31 @@
+import classes from './comment-list.module.css';
+
+function CommentList(props) {
+    const { items } = props;
+    return (
+        <ul className={classes.comments}>
+            {items.map((item) => (
+                <li key={item._id}>
+                    <p>{item.text}</p>
+                    <div>
+                        By <address>{item.name}</address>
+                    </div>
+                </li>
+            ))}
+            <li>
+                <p>My comment is amazing!</p>
+                <div>
+                    By <address>Maximilian</address>
+                </div>
+            </li>
+            <li>
+                <p>My comment is amazing!</p>
+                <div>
+                    By <address>Maximilian</address>
+                </div>
+            </li>
+        </ul>
+    );
+}
+
+export default CommentList;
