8d5774068cc14ed4bc302003a43ddf4d6e3ed24f
wskim <kws60000@naver.com>
2022-11-06 09:11:51 +0900

    TIL: 초기 프로젝트 생성

---
+++ blog_prj/pages/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function HomePage(props) {
+    return (
+        <Fragment>
+            <Hero />
+        </Fragment>
+    );
+}
+
+export default HomePage;
---
+++ blog_prj/pages/posts/[slug].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PostDetailPage(props) {
+    return <div></div>;
+}
+
+export default PostDetailPage;
